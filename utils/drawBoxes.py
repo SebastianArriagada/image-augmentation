4b0b2e7479fd47b67ce866e10d6b262122e02216
Sebastian Arriagada <sarriagada15@gmail.com>
2022-01-06 04:57:57 +0100

    Adjust little plot parameters

--- utils/drawBoxes.py
+++ utils/drawBoxes.py
@@ -2,7 +2,7 @@ import cv2
 import random 
 
 def drawBoxes(image, labels):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
     for label in labels:
         cx = round(label[1] * image.shape[1])
         cy = round(label[2] * image.shape[0])
@@ -15,8 +15,8 @@ def drawBoxes(image, labels):
 
         random_color = (random.randint(0,255) ,random.randint(0,255) ,random.randint(0,255) )
 
-        image = cv2.circle(image, center = (cx,cy), radius = 3, color = random_color)
-        image = cv2.rectangle( image, pt1 = (x1,y1), pt2 = (x2,y2), color= random_color, thickness= 2)
+        image = cv2.putText(image, label[0], (cx - 5, cy - 5), cv2.FONT_HERSHEY_SIMPLEX, 3, random_color, 6, cv2.LINE_AA)
+        image = cv2.rectangle( image, pt1 = (x1,y1), pt2 = (x2,y2), color= random_color, thickness= 4)
 
     return image
     #print(cx,cy,w,h)
